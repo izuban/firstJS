1903ecee0f835a020b272c20292e01527831afe2
IHOR Zuban <ihor.zuban@gmail.com>
2023-08-30 12:54:20 +0200

    return the age

--- randomHuman.js
+++ randomHuman.js
@@ -1,8 +1,5 @@
 import { faker } from '@faker-js/faker';
 
-
-
-
 class randomHuman {
     constructor(name, surname, dateOfBirth, height, weight) {
         this.name = faker.person.firstName()
@@ -10,5 +7,15 @@ class randomHuman {
         this.dateOfBirth = faker.date.birthdate()
         this.height = faker.number.int({ min: 100, max: 200 })
         this.weight = faker.number.float({ min: 40, max: 100, precision: 0.001 })
+    }
+    getAge() {
+        let today = new Date();
+        let birthDate = new Date(this.dateOfBirth);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        let m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
     }
 }
